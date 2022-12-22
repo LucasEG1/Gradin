4a0da32604bb54070e841de8d91a2027ffb37608
Lucas Gyori <gyori.daw@gmail.com>
2022-12-22 20:19:24 +0100

    Check cambiado a @GetMapping

--- src/main/java/com/lucas/Gradin/api/AuthController.java
+++ src/main/java/com/lucas/Gradin/api/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +33,7 @@ public class AuthController {
         return new ResponseEntity<ProfesorEntity>(oAuthService.login(oProfesorBean), HttpStatus.OK);
     }
 
-    @PostMapping
+    @GetMapping
     public ResponseEntity<ProfesorEntity> check() {
         return new ResponseEntity<ProfesorEntity>(oAuthService.check(), HttpStatus.OK);
     }
